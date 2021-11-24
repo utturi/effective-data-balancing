1d7a4080d2b229d3a748f3c017d68190347de641
huhji <huhji.elha@gmail.com>
2021-11-24 15:19:11 +0900

    Modify model's confidence score & Add json returns

--- yolov5_object_detection/detect_image.py
+++ yolov5_object_detection/detect_image.py
@@ -1,51 +1,63 @@
 import torch
 import os
 import s3_access
+import json
+from PIL import Image
 from flask import Flask, request
 
-app = Flask(__name__)
 
+def detect(source=None):
+    model = torch.load("weights/yolov5_detection_weights.pt")
+    model.conf = 0.6  # NMS confidence threshold
+    model.iou = 0.45
+    if not source:
+        source = "sample.jpg"
 
-def detect(source=None, save_img=True):
-    source_bucket = s3_access.get_s3_bucket()
-    # 3.s3이미지 받아와서 로컬에 저장할 경로 이미지 만들 때
-    file_path = os.path.join("/app", source)
+    img = Image.open(source)
+    w, h = img.size
 
-    # 4.s3에서 받아오고 로컬에 저장
-    s3_access.download_file(source_bucket, source, file_path)
-
-    # 5.저장된 이미지로 모델 업로드 및 실행
-    model = torch.load("weights/yolov5_object_detection.pt")
-    Model_input_image = [file_path]
+    Model_input_image = [img]
     results = model(Model_input_image, size=640)
+    results_pandas = results.pandas().xyxy[0]
 
-    path, filename = "/app", "image0.jpg"
+    save_dict = {"result": {"width": w, "height": h, "objects": []}}
+    save_labels = []
+
+    for i in range(len(results_pandas)):
+        out = results_pandas.loc[i]
+        object_dict = {"attributes": {}}
+        object_dict["attributes"][out["name"]] = float(out["confidence"])
+        object_dict["left"] = out["xmin"]
+        object_dict["top"] = out["ymin"]
+        object_dict["right"] = out["xmax"]
+        object_dict["bottom"] = out["ymax"]
+        save_dict["result"]["objects"].append(object_dict)
+        save_labels.append(f"{out['name']}: {out['confidence']}")
+
     # 6.파일경로와 확장자 분리 origin_name = /test/image0
-    origin_name, ext = os.path.splitext(filename)
+    origin_name, _ = os.path.splitext(source)
 
     # 7.모델 결과 저장할 경로 지정 및 저장
-    output_file_name = origin_name + "_output" + ext
-    model_output_save_path = os.getcwd()
-    output_save_path = os.getcwd() + "/image0.jpg"
-    results.save(model_output_save_path)
+    results.save("output")
+    output_file_path = os.path.join("output", source)
 
     # 8.json형식 아웃풋 저장 변수
-    json_input_data = results.pandas().xyxy[0]
+    # json_input_data = results.pandas().xyxy[0]
 
     # 9.json 저장 파일 경로지정.
     json_file_name = origin_name + "_output.json"
     # 10.json 파일로 저장
-    json_input_data.to_json(json_file_name, orient="table")
-    json_save_path = os.path.join(path, json_file_name)
+    with open(json_file_name, "w", encoding="utf-8") as f:
+        json.dump(save_dict, f, ensure_ascii=False)
 
     client = s3_access.get_s3_client()
-    s3_access.upload_file(source_bucket, output_save_path, output_file_name)
-    s3_access.upload_file(source_bucket, json_save_path, json_file_name)
+    s3_access.upload_file(client, output_file_path, source)
+    s3_access.upload_file(client, json_file_name, json_file_name)
 
     res_json = {
-        "img_url": s3_access.get_public_url(client, output_file_name),
+        "img_url": s3_access.get_public_url(client, source),
         "json_url": s3_access.get_public_url(client, json_file_name),
-        "text_arr": res_label,
+        "text_arr": save_labels,
     }
 
-    return output_file_name
+    return res_json
---
+++ yolov5_object_detection/tmp.py
@@ -0,0 +1,5 @@
+import detect_image
+
+file_name = None
+response = detect_image.detect(source=file_name)
+print(response)
